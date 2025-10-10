2f6025bb63359e9d82050d1e8426d8fc7dead3fa
kantu <118933440+kantucoaquira@users.noreply.github.com>
2025-10-10 18:57:27 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/upeu/sysasistencia/repositorio/IFacultadRepository.java
@@ -0,0 +1,8 @@
+package pe.edu.upeu.sysasistencia.repositorio;
+
+import pe.edu.upeu.sysasistencia.modelo.Facultad;
+import java.util.Optional;
+
+public interface IFacultadRepository extends ICrudGenericoRepository<Facultad, Long>{
+    Optional<Facultad> findByNombre(String nombre);
+}
---
+++ src/main/java/pe/edu/upeu/sysasistencia/repositorio/ISedeRepository.java
@@ -0,0 +1,8 @@
+package pe.edu.upeu.sysasistencia.repositorio;
+
+import pe.edu.upeu.sysasistencia.modelo.Sede;
+import java.util.Optional;
+
+public interface ISedeRepository extends ICrudGenericoRepository<Sede, Long>{
+    Optional<Sede> findByNombre(String nombre);
+}
